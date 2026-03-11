4e4761f54236f8ae491f8c7a539949861d927cff
ishuraj1919-ai <ishuraj1919@gmail.com>
2026-03-11 22:28:24 +0530

    learn.py and fuction.py
    first commit

---
+++ function.py
@@ -0,0 +1,4 @@
+#ask for name 
+name = input ("what is your name ?")
+#prinnt greeting with name 
+print ("hello", name)
---
+++ learn.py
@@ -0,0 +1,7 @@
+# Simple Hello World program in Python
+
+def main():
+    print("Hello, World!")  # Output message
+
+if __name__ == "__main__":
+    main()
