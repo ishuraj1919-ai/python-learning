bdd01819408c86d0b05ad38724b4b6d762c3f03a
ishuraj1919-ai <ishuraj1919@gmail.com>
2026-03-13 04:02:25 +0530

    for writing short self introduction

---
+++ short-introduction.py
@@ -0,0 +1,20 @@
+""" THIS PROGRAM WILL WRITE A SHORT INTRODUCTION ABOUT A PERSON """
+#Ask for name
+Name = input("what's your name ?")
+#print greeting with name
+print ("hello",Name)
+
+#Ask for city
+City = input("what's your City name?")
+
+#Ask for gender
+GENDER = input("WHAT IS YOUR GENDER ?")
+#Ask for age
+age = input("whta's your age")
+#Ask for hobby
+HObby = input("hobby?")
+#Print the introduction
+print("I AM",Name)
+print("I LIVE IN",City)
+print("I am ",age,"years old")
+print("my hobby is",HObby)
