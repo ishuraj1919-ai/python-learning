a643ebd15a571a0371cbb3b8bd292478a8d19c59
ishuraj1919-ai <ishuraj1919@gmail.com>
2026-03-14 22:57:02 +0530

    proble 1 on symbol

---
+++ problem-1-symbol.py
@@ -0,0 +1,10 @@
+"""use \n & \t ,to print your name,age,and city on a line with tab space """
+# solution
+#ask for name
+name = input("what's your name?")
+#ask for age
+age = input("what's your age?")
+#ask for city
+city = input("what's your city?")
+#print the introduction
+print(f"name=\t{name},\nage=\t{age},\ncity=\t{city}")
