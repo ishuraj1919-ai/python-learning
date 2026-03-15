bc671967776857b6a59e53a6ecae16956112212f
ishuraj1919-ai <ishuraj1919@gmail.com>
2026-03-15 09:35:32 +0530

    problem 5

---
+++ problem-5-symbol.py
@@ -0,0 +1,12 @@
+"""use + - / // % **
+take any two number and print:
+-their sum
+-their difference
+-their produt
+-their devision
+-their floor devision
+-their remainder
+-first no to the power of 2nd"""
+x = 32
+y = 12
+print(f"{x+y}\n{x-y}\n{x*y}\n{x/y}\n{x//y}\n{x%y}\n{x**y}")
