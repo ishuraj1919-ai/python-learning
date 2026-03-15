c1214e0d98a021147a923b4c2e5d3e844af7db25
ishuraj1919-ai <ishuraj1919@gmail.com>
2026-03-15 07:05:19 +0530

    2nd problem

---
+++ problem-2-symbol.py
@@ -0,0 +1,9 @@
+"""use + and f
+create two variables: first_name and join them and print full name"""
+# solution
+#ask for first_Name
+First_Name = input("ask for First_Name ")
+#ask for 2nd name
+Second_Name = input("ask for second name")
+#print full name
+print(f"{First_Name+" "+Second_Name}")
